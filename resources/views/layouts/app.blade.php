1047eef4bfa8d7eced0a79c67192eb5338452f41
Manyi Fonseca <bellafon663@gmail.com>
2024-07-07 15:08:26 -0300

    reconfiguracion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('titulo','Diesel.Urquiza')</title>
-     @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('titulo','Diesel Urquiza')</title>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>
-     {{-- Navbar --}}
-     @include('layouts.navbar')
-     <div class="text-end text-md text-white-100 mx-6">
-        Bienvenido a Nuestra Tienda!
-     </div>
+        {{-- Navbar --}}
+        @include('layouts.navbar')
     </header>
-<main>
-    <div class="bg-blue-100 my-4 text-center">
-          <h1 class="text-1g font-semibold m-4 uppercase">@yield('cabecera')</h1>
-    </div>
-   @yield('contenido')
-</main>
+    <main>
+        <div class="bg-green-100 my-4 text-center">
+            <h1 class="text-lg font-semibold m-4 uppercase">@yield('cabecera')</h1>
+        </div>
+        @yield('contenido')
+    </main>
     <footer class="footer items-center p-4 bg-neutral text-neutral-content">
-      @include('layouts.footer')
+        @include('layouts.footer')
     </footer>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BombaController;
-
+use App\Http\controllers\bombacontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +15,18 @@ use App\Http\Controllers\BombaController;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/','welcome')->name('home');
 
-//CRUD de Bombas   
-Route::resource('bombas', BombaController::class);// crear 7 rutas para CRUD 
+Route::resource('bombas', bombacontroller::class)->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
